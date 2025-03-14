3ad9f280a0728f13a05f84758f1e9cd3f7e8d9df
handwithfivefingers <truyenmv@flomail.net>
2025-03-14 13:51:36 +0700

    fix Button type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,10 +6,10 @@ interface IButton extends BaseProps, AllHTMLAttributes<HTMLButtonElement> {
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const Button = ({ className, children, ...props }: IButton) => {
+export const Button = ({ className, children, type = "button", ...props }: IButton) => {
   return (
     <button
-      type="button"
+      type={"button"}
       className={clsx("px-2 py-1 rounded  bg-slate-700 cursor-pointer text-sm shadow", className)}
       {...props}
     >
